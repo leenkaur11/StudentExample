f9f2453fd3175b7ab12e4c54e03a6919328547ce
Gurleen Kaur <kau16207@sheridancollege.ca>
2024-09-23 13:31:06 -0400

    first commit

---
+++ src/studentexample/Student.java
@@ -0,0 +1,35 @@
+package studentexample;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author gurleenmomi
+ */
+public class Student {
+     private String name;
+    private int age;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
